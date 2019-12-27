fa87c9d65fdbcf759d51209d1924bb186d45cc02
Zackery Ayscue <ztayscue@gmail.com>
2019-12-27 00:12:03 -0500

    quick fix

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const todoItemRepository_1 = require("./persistance/todoItemRepository");
-const todoItems = new todoItemRepository_1.TodoItemRepository();
-setInterval(() => todoItems.getAll().then(results => console.log(JSON.stringify(results))), 1000);
-//# sourceMappingURL=server.js.map
